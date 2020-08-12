faf1cef41aa9ba0f7dd7bb44b8c1b4cfeda5fd0b
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-08-12 11:32:05 +0300

    Add files via upload

---
+++ meme/js/service.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [{
+        txt: 'I never eat Falafel',
+        size: 20,
+        align: 'left',
+        color: 'red'
+    }]
+}
